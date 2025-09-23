098d11cc34246ff727a6ff7738bebbdd30ba5371
Martín Bocanegra <martin@uin.tech>
2025-09-23 11:48:57 -0300

    add eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,7 +28,10 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      "unused-imports/no-unused-imports": "warn",
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-empty-object-type": "off"
     },
   },
 );
