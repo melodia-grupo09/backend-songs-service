79dced7a9b20d4c581b96f7d414d318e2e3ea26f
Martín Bocanegra <martin@uin.tech>
2025-09-23 14:18:24 -0300

    add missing modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import mikroOrmConfig from 'mikro-orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MikroOrmModule.forRoot({
+      ...mikroOrmConfig,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
